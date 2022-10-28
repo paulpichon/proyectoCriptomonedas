5e37d1025f73380254f64558a30046a2e19f14a6
Paul Pichon F <paul10_barca@hotmail.com>
2022-10-27 22:47:10 -0500

    validacion de formulario

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 //VARIABLES
 //select para elegir la criptomoneda
 const criptoMonedasSelect = document.querySelector('#criptomonedas');
+//input moneda
+const monedaSelect = document.querySelector('#moneda');
+//variable para el formulario
+const formulario = document.querySelector('#formulario');
+
+//creamos un objeto vacio
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
+
 
 //creamos un Promise
 const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
@@ -12,6 +23,12 @@ const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
 document.addEventListener('DOMContentLoaded', () => {
     //llamar funcion para consultar criptomonedas
     consultarCriptomonedas();
+
+    //listener a formulario
+    formulario.addEventListener('submit', submitFormulario);
+
+    //listener a input criptomoneda
+    criptoMonedasSelect.addEventListener('change', leerValor);
 });
 //funcion para consultar las criptomonedas
 function consultarCriptomonedas() {
@@ -41,4 +58,29 @@ function selectCriptomonedas( criptomonedas ) {
         //renderizar
         criptoMonedasSelect.appendChild( option );
     });
+}
+//funcion para leer el valor del objeto de busqueda
+function leerValor( e ) {
+    //recuperamos el valor del input y lo asignamos al objeto 
+    objBusqueda[ e.target.name ] = e.target.value;
+    //console.log( objBusqueda );
+}
+
+//funcion para validar formulario
+function submitFormulario( e ) {
+    //prevenir la accion por defecto
+    e.preventDefault();
+    //destructuring
+    const { moneda, criptomoneda } = objBusqueda;
+    //validar si no estan vacios los campos
+    if ( moneda === '' || criptomoneda === '') {
+        //llamamos funcion para mostrar alerta
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+
+}
+//funcion para mostrar alerta
+function mostrarAlerta( mensaje ) {
+    console.log( mensaje );
 }
